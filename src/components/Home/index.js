443944c5d4e7044ab4aa4115e9c19cd87a427205
1chary <147873086+1chary@users.noreply.github.com>
2024-07-05 16:19:15 +0530

    Update index.js
    removed un used data

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,13 +2,7 @@ import { Component } from "react";
 import "./index.css"
 import {Content,Cell, Column, Row, TableView, TableBody, TableHeader,Provider, defaultTheme} from '@adobe/react-spectrum'
 
-const airportData = [
-    {id: 1,name : "Indira Gandhi International Airport", country: "India", code: "DEL", terminals: "3"},
-    {id: 2,name : "Dubai International Airport", country: "UAE", code: "DXB", terminals: "5"},
-    {id: 3,name : "Heathrow Airport", country: "England", code: "LHR", terminals: "6"},
-    {id: 4,name : "Istanbul Airport", country: "Turkey", code: "IST", terminals: "3"},
-    {id: 5,name : "Rajiv Gandhi International Airport", country: "USA", code: "DFW", terminals: "14"},
-]
+
 
 class Home extends Component {
     render() {
